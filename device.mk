fe42ac56ab8a9237b1453265a0a0e333c3eca56a
Rve <rve27github@gmail.com>
2024-08-02 07:25:21 +0700

    beryllium: Inherit Miui Camera
    Signed-off-by: Rve <rve27github@gmail.com>

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/beryllium/beryllium-vendor.mk)
 
+# Miui Camera
+$(call inherit-product-if-exists, vendor/miuicamera/config.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2246
 TARGET_SCREEN_WIDTH := 1080
