48dad9b714f2e2359d53d420d05c08a8acd8e790
Rve <rve27github@gmail.com>
2024-08-05 01:33:12 +0700

    beryllium: Serasa-ify
    Signed-off-by: Rve <rve27github@gmail.com>

--- serasa_beryllium.mk
+++ serasa_beryllium.mk
@@ -6,11 +6,15 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Serasa stuff.
+$(call inherit-product, vendor/serasa/config/common_full_phone.mk)
+
+# Serasa
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_DISABLE_EPPE := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := serasa_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# SDM845 Device
+git clone --depth=1 -b serasa-13 https://github.com/Rve27/android_device_xiaomi_sdm845-common.git device/xiaomi/sdm845-common
+
+# Beryllium Vendor
+git clone --depth=1 -b serasa-13 https://github.com/Rve27/proprietary_vendor_xiaomi_beryllium.git vendor/xiaomi/beryllium
+
+# SDM845 Vendor
+git clone --depth=1 -b serasa-13 https://github.com/Rve27/proprietary_vendor_xiaomi_sdm845-common.git vendor/xiaomi/sdm845-common
+
+# RvKernel
+git clone --depth=1 -b be4 https://github.com/Rve27/RvKernel-Beryllium.git kernel/xiaomi/sdm845
+
+# MIUI Camera
+git clone --depth=1 -b 13 https://github.com/Rve27/vendor_miuicamera.git vendor/miuicamera
