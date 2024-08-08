cb9a0136c2cc9d3582d21cd954043ce6929fc8ba
Rve <rve27github@gmail.com>
2024-08-08 18:29:03 +0700

    beryllium: ewOS-ify
    Signed-off-by: Rve <rve27github@gmail.com>

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -6,11 +6,16 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# ewOS
+CUSTOM_MAINTAINER := Rve
+TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# SDM845 Common Device
+git clone --depth=1 -b ew-14 https://github.com/Rv-Trees/android_device_xiaomi_sdm845-common.git device/xiaomi/sdm845-common
+
+# Beryllium Vendor
+git clone --depth=1 -b 14 https://github.com/Rv-Trees/proprietary_vendor_xiaomi_beryllium.git vendor/xiaomi/beryllium
+
+# SDM845 Common Vendor
+git clone --depth=1 -b 14 https://github.com/Rv-Trees/proprietary_vendor_xiaomi_sdm845-common.git vendor/xiaomi/sdm845-common
+
+# SDM845 Kernel
+git clone --depth=1 -b 14 https://github.com/Rv-Trees/android_kernel_xiaomi_sdm845.git kernel/xiaomi/sdm845
+
+# MIUI Camera Beryllium
+git clone --depth=1 -b 14 https://github.com/Rv-Trees/vendor_miuicamera vendor/miuicamera
